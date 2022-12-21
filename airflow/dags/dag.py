d6cf4c08fd9296af53ea0ee5ab50d3f9e235943b
Fred <fred.mny@gmail.com>
2022-12-21 07:36:51 -0300

    refactor: major changes to project

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -1,26 +1,37 @@
 from datetime import datetime, timedelta
 import os
 from airflow import DAG
-from operators import
+from operators.process_data_spark import processDataSpark
 
-from helpers import SparkFunctions
+from helpers.spark_functions import SparkFunctions
 
 spark_functions = SparkFunctions()
+input_path='s3a://fw-flights-source'
+output_path='s3a://fw-flights-tables'
 
 default_args = {
   'owner': 'fred.waldow',
   'depends_on_past': False,
-  'start_date': datetime(2009, 01, 01),
+  'start_date': datetime(2009, 1, 1),
   'retries': 3,
   'retry_delay': timedelta(minutes=5),
   'email_on_retry': False,
-  'catchup': False
+  'catchup': False,
+  'catchup_by_default': False,
 }
 
 dag = DAG(
   'flights',
   default_args=default_args,
   description='Transforms data to analyze flights data',
-  schedule_interval='0 3 * * *'
+  # schedule_interval='0 3 * * *'
+)
+
+process_airlines = processDataSpark(
+  task_id='process_airlines',
+  dag=dag,
+  input_path=input_path,
+  output_path=output_path,
+  spark_function=spark_functions.process_airlines_data
 )
 
--- airflow/plugins/helpers/spark_functions.py
+++ airflow/plugins/helpers/spark_functions.py
@@ -11,7 +11,6 @@ class SparkFunctions:
       header=True
     )
 
-    
     values_to_exclude = ['&T', '++', '-+', '--', '..', '-']
     dim_airlines = stg_airlines.selectExpr(
       '`Airline ID` as id', # will be used to deduplicate the iata codes
@@ -42,4 +41,70 @@ class SparkFunctions:
     dim_airlines = dim_airlines.drop('row_n', 'id')
 
     dim_airlines.write.partitionBy('country').mode('overwrite')\
-      .parquet(os.path.join(output_path, dim_table))
+    .parquet(os.path.join(output_path, dim_table))
+      
+  def process_airports_data(spark, input_path, output_path):
+    airports_path = 'airports/airports-codes_json.json'
+    dim_table = 'dim_airports'
+
+    stg_airports = spark.read.json(
+      os.path.join(input_path, airports_path)
+    )
+    
+    dim_airports = stg_airports.selectExpr(
+      'iata_code',
+      'name',
+      'INT(elevation_ft) as elevation',
+      'gps_code',
+      'iso_country',
+      'iso_region',
+      'municipality',
+      'type',
+      'FLOAT(SPLIT(coordinates, ",")[0]) as longitude',
+      'FLOAT(SPLIT(coordinates, ",")[1]) as latitude'
+    ).filter(
+      F.col('iata_code').isNotNull() & \
+      ~F.col('iata_code').isin('-', '0')
+    )
+    
+    # Creating a map and then apply it to the type column 
+    # to later filter out duplicates
+    airport_mapping = {
+      'large_airport': '1',
+      'seaplane_base': '5',
+      'heliport': '4',
+      'closed': '6',
+      'medium_airport': '2',
+      'small_airport': '3'
+    }
+
+    dim_airports = dim_airports.withColumn(
+      'airport_code',
+      F.col('type')
+    ).replace(
+      to_replace=airport_mapping,
+      subset=['airport_code']
+    )
+
+    dim_airports = dim_airports.withColumn(
+      'airport_code',
+      dim_airports['airport_code'].cast('integer')
+    )
+    
+    # Window function to late filter the results based on condition
+    w = Window \
+      .partitionBy('iata_code') \
+      . orderBy('airport_code')
+
+    # Apply window function
+    dim_airports = dim_airports.withColumn(
+      'row_n',
+      F.row_number().over(w)
+    )
+
+    # Filter rows based on applied order
+    dim_airports = dim_airports.filter(F.col('row_n') == 1)
+    dim_airports = dim_airports.drop('row_n', 'airport_code')
+    
+    dim_airports.write.partitionBy('iso_country').mode('overwrite')\
+    .parquet(os.path.join(output_path, dim_table))
